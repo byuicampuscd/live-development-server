50eb28e1913b3f237f0bd603d22b0328422bff8d
Joshua McKinney <mckinneyj@byui.edu>
2017-03-03 13:29:29 -0700

    can send cool.html file as html.js

--- launch.js
+++ launch.js
@@ -2,6 +2,7 @@
 
 
 var fs = require("fs"),
+    path = require("path"),
     https = require("https"),
     url = require("url"),
     chalk = require('chalk'),
@@ -21,14 +22,51 @@ function getScript(port) {
 }
 
 function processRequest(req, res) {
-    var request = url.parse(req.url).pathname;
+    var request = url.parse(req.url).pathname,
+        fileData = path.parse(request);
+    
     var reader = fs.createReadStream("." + request);
     reader.pipe(res);
     console.log(request);
     reader.on("error", function () {
-        res.writeHead(404);
-        res.write("404:\nThe page you are looking for does not exist...\nSO GET LOST!\neven though you already are...");
-        res.end();
+        
+        function realError(){
+            res.writeHead(404);
+            res.write("404:\nThe page you are looking for does not exist.");
+            res.end();  
+        }
+        
+        //check if the file was js and if an html file with the same name exsits
+        if(fileData.ext === ".js"){
+            var htmlFileName = path.join('.', fileData.dir, fileData.name + '.html');
+            console.log('Checking for:', htmlFileName);
+            fs.readFile(htmlFileName, 'utf8', function(err, data){
+                if(err){
+                    realError();
+                    return;
+                }
+                var encoded = encodeURIComponent(data),
+                
+                //make string
+                fileOut = `var ${fileData.name}Text = '${encoded}';\n\n`;
+                
+                //make div and set values
+                fileOut += `var ${fileData.name}Div = document.createElement('div');\n`;
+                fileOut += `${fileData.name}Div.setAttribute('id','${fileData.name}')\n`;
+                fileOut += `${fileData.name}Div.innerHTML = de  codeURIComponent(${fileData.name}Text)\n`;
+                
+                //put it in body
+                fileOut += `document.body.appendChild(${fileData.name}Div)`;
+                
+                //send it back
+                res.end(fileOut);
+                console.log("sent:", htmlFileName);
+            });
+            
+        } else {
+            realError();
+        }
+        
     });
 }
 
